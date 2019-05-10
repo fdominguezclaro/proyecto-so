6415b600ea3d3e9b57cbc10e0be6c7624705de12
fdominguezc <fdominguezc@uc.cl>
2019-05-10 17:54:41 -0400

    add basic files

---
+++ crfs/cr_API.c
@@ -0,0 +1,66 @@
+#include "cr_API.h"
+
+void cr_mount(char* diskname)
+  {
+
+  }
+
+void cr_bitmap()
+  {
+
+  }
+
+int cr_exists(char* path)
+  {
+
+  }
+
+void cr_ls(char* path)
+  {
+
+  }
+
+int cr_mkdir(char *foldername)
+  {
+
+  }
+
+crFILE* cr_open(char* path, char mode)
+  {
+
+  }
+
+int cr_read(crFILE* file_desc, void* buffer, int nbytes)
+  {
+
+  }
+
+int cr_write(crFILE* file_desc, void* buffer, int nbytes)
+  {
+
+  }
+
+int cr_close(crFILE* file_desc)
+  {
+
+  }
+
+int cr_rm(char* path)
+  {
+
+  }
+
+int cr_hardlink(char* orig, char* dest)
+  {
+
+  }
+
+int cr_unload(char* orig, char* dest)
+  {
+
+  }
+
+int cr_load(char* orig)
+  {
+
+  }
---
+++ crfs/cr_API.h
@@ -0,0 +1,31 @@
+#pragma once
+
+typedef struct FILE {
+    int value;
+} crFILE;
+
+void cr_mount(char* diskname);
+
+void cr_bitmap();
+
+int cr_exists(char* path);
+
+void cr_ls(char* path);
+
+int cr_mkdir(char *foldername);
+
+crFILE* cr_open(char* path, char mode);
+
+int cr_read(crFILE* file_desc, void* buffer, int nbytes);
+
+int cr_write(crFILE* file_desc, void* buffer, int nbytes);
+
+int cr_close(crFILE* file_desc);
+
+int cr_rm(char* path);
+
+int cr_hardlink(char* orig, char* dest);
+
+int cr_unload(char* orig, char* dest);
+
+int cr_load(char* orig);
