0c8376871ed075e71877ecd72fabfc35184766b2
fdominguezc <fdominguezc@uc.cl>
2019-05-23 02:19:11 -0400

    feat(cr_rm): ready

--- src/crfs/functions/functions.h
+++ src/crfs/functions/functions.h
@@ -13,7 +13,7 @@ int byteToBits(unsigned char byte);
 
 Graph *load_disk(void);
 
-/** Recorta un string insertando un caractre de termino
+/** Recorta un string insertando un caracter de termino
  * Obtenido de: https://stackoverflow.com/questions/27414696/remove-last-four-characters-from-a-string-in-c
 */
 void trim_end(char *str, int n);
@@ -28,3 +28,12 @@ void write_dir_block(unsigned int index, Dir_parser* dir);
 
 /** Escribe en el bitmap el estado de un bloque dado */
 void write_bitmap(unsigned int index, unsigned int value);
+
+/** Escribe un byte en el bloque index y el offset dado */
+void write_byte(unsigned int index, int offset, unsigned char value);
+
+/** Escribe 4 bytes en el bloque index y el offset dado */
+void write_4bytes(unsigned int index, int offset, unsigned int value);
+
+/** Lee y retorna un bloque indice */
+Index_block *read_index_block(unsigned int index);
